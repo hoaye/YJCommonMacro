5877ea7e9507c4b3d615d48bd3016b4359bd7a17
yuejun.hou <8069060@qq.com>
2017-05-27 13:48:48 +0800

    566

--- YJCommonMacro/YJCommonMacro.h
+++ YJCommonMacro/YJCommonMacro.h
@@ -57,7 +57,7 @@
 
 //重写NSLog,Debug模式下打印日志和当前行数
 #if DEBUG
-#define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#define NSLog(FORMAT, ...) fprintf(stderr,"Function:%s Line:%d\n Content:%s\n\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
 #else
 #define NSLog(FORMAT, ...) nil
 #endif
