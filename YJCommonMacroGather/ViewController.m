95186ad25e9ba9a47f03c77a527a25ec8dbf9108
yj.hou <houmanager@qq.com>
2018-04-12 11:51:14 +0800

    update

--- YJCommonMacroGather/ViewController.m
+++ YJCommonMacroGather/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YJCommonLogging.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    YJLog(@"---");
+    COLog(@"+++");
     
 }
 
